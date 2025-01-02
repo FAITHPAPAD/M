3093f952826ea5f4a8688e45ea8c83b5b113c618
FAITHPAPAD <waji.dfakeyt@gmail.com>
2025-01-02 21:33:29 +0530

    Update def.py

--- def.py
+++ def.py
@@ -10,10 +10,10 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 # Insert your Telegram bot token here
-BOT_TOKEN = '7471974772:AAEeNXUN6VkCpDBHWWlUzk9aYmlFbP36oO4'
+BOT_TOKEN = '7748076089:AAGuiDwnRgDNvlcwQcegfaeyg-m0jQT6KzQ'
  
 # Admin user IDs
-ADMIN_IDS = {"5324974425"}
+ADMIN_IDS = {"6401539018"}
 
 
 USER_FILE = "users.json"
